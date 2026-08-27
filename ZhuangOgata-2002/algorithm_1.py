e4d0f7d492228d53a581765c45c386b0d75919b9
Gregoria <gm.andrea121@gmail.com>
2026-08-27 17:55:52 -0600

    Claridad en el ciclo while de los pasos 3 al 6

--- ZhuangOgata-2002/algorithm_1.py
+++ ZhuangOgata-2002/algorithm_1.py
@@ -12,14 +12,14 @@ from scipy.optimize import minimize
 
 
 class ETAS_Declustering:
-    def __init__(self, archivo, M0=4.3, d=0.2, epsilon=10**(-3)):
+    def __init__(self, archivo, M0=4.3, d=0.2, epsilon=10**(-3), max_iter=100):
         self.archivo = archivo
         self.M0 = M0
         self.d = d
         self.epsilon = epsilon  
-
-        # Inicializar
         self.N = 0
+        self.max_iter = max_iter
+        self.convergence_df = pd.DataFrame(columns = ["log L", "v", "A", "c", "alpha", "p", "d"])
 
         self.df = self.read_csv(self.archivo)
 
@@ -31,44 +31,39 @@ class ETAS_Declustering:
         d = self.calculate_bandwidth()
 
         # 2. Set l = 0 and u^{(0)}(x,y) = 1
-        u_xy = np.asarray([], dtype=int)  # Here we have to check if u is an array of int
+        self.u_xy = np.asarray([], dtype=int)  # Here we have to check if u is an array of int
 
         self.l = 0  # For iterations
-        u_xy = np.append(u_xy, 1)
-        """
+        self.u_xy = np.append(self.u_xy, 1)
 
-
-        convergence_steps = pd.DataFrame(columns = ["log L", "v", "A", "c", "alpha", "p", "d"])
-        # 3. Using the maximum likelihood procedure, fit the conditional
-        #   intensity function λ(t,x,y|Ht) = vu^{(1)}(x,y) + 
-        #                               \sum_{k:tk<t}κ(Mk)g(t-tk)*f(x-xk,y-yk|Mk)
-        # to the earthquake data.
-        while condition == True:
-            fci = self.fit_conditional_intensity(t, x, y, Ht, v=1)
+        condition = True
+        while condition == True and self.l < self.max_iter:
+            # 3. Using the maximum likelihood procedure, fit the conditional
+            #   intensity function λ(t,x,y|Ht) = vu^{(1)}(x,y) + 
+            #                               \sum_{k:tk<t}κ(Mk)g(t-tk)*f(x-xk,y-yk|Mk)
+            # to the earthquake data.
+            self.fit_conditional_intensity()
 
             # 4. Calculate ρj for each j=1,2,...,N
-
-            for j in range(1,N):
-                p[j] = self.calculate_pj()
-
+            self.calculate_pj()
 
             # 5. Calculate μ(x,y) and record as u^{l+1}(x,y)
-                u_xy[l+1] = self.calculate_mu_estim()
+            mu = self.calculate_mu_estim()
+            self.u_xy.append(self.u_xy, mu)
 
             # 6. If max_{(x,y)}|u^{l+1}(x,y)-u^{l}(x,y)|> ε, where ε is a small positive
             # number , then set l = l + 1 and go to step 3. Otherwise, take 
             # v*u^{l+1}(x,y) as the background rate and stop.
-                epsilon = 10**(-3)
-                v= 20
-                if np.abs(u_xy[l+1]-u_xy[l])>epsilon:
-                    l = l+1
 
-                else:
-                    v * u_xy[l+1]
-                    condition = False
+            self.difference = self.calculate_difference()
 
-        print(convergence_steps)"""
+            if self.difference > self.epsilon:
+                condition = False
 
+            l +=1
+
+        self.background_rate = self.u_xy[-1]
+        print(self.convergence_df)
 
     def read_csv(self, file):
         return pd.read_csv(file)
@@ -84,7 +79,7 @@ class ETAS_Declustering:
         # self.n_p is involved in the calculation of dj, but I set de degree value as the article
         self.dj = np.full(self.N, self.d)
 
-    def kapa(self, M, A, alpha, M0):
+    def kappa(self, M, A, alpha, M0):
         return A * np.exp(alpha * (M - M0))
 
     def g(self, t, c, p):
@@ -102,7 +97,7 @@ class ETAS_Declustering:
         other_shocks_intensity = 0
         for tk in Ht:
             if tk < t:
-                other_shocks_intensity += kapa(M[k]) * g(t-tk) * f(x-x[k], y-y[k], M[k], d, M0, alpha)
+                other_shocks_intensity += self.kapa(M[k]) * g(t-tk) * f(x-x[k], y-y[k], M[k], d, M0, alpha)
 
         lamb = main_shocks_intensity + other_shocks_intensity
 
@@ -114,7 +109,7 @@ class ETAS_Declustering:
 
     def calculate_pj(self):
         pij = np.array([N])
-        pij = kapa(Mi) * g(tj-ti) * f(xj-xi, yj-yi, Mi) / fci(tj, xj, yj, H_tj)
+        pij = self.kappa(Mi) * self.g(tj-ti) * self.f(xj-xi, yj-yi, Mi) / fci(tj, xj, yj, H_tj)
         pj = np.sum(pij) 
         return pj
 
