9b55b16562fcd07f45b9aac0a8d72f95dc1cc3ab
Gregoria <gm.andrea121@gmail.com>
2026-09-01 12:57:45 -0600

    Dataframe reindexado previo a colorear con la intensidad de fondo

--- ZhuangOgata-2002/map_of_Mexico.py
+++ ZhuangOgata-2002/map_of_Mexico.py
@@ -17,15 +17,18 @@ def prepare_data(file_earthquakes, file_uxy, M0):
     u_xy_series = pd.read_csv(file_uxy)
 
     df = df[df['Magnitude']>= M0].copy()
+    df.reset_index(drop=True, inplace=True)
+    print(df.head())
+
     df['u_xy'] = u_xy_series['u_xy']
 
     return df
 
 def show_results(df: pd.DataFrame):
-    fig = px.scatter_map(df, lat=df["Latitude"], lon=df["Longitude"],
-                         hover_name=df["Magnitude"],  # text that appears over 
-                         hover_data=df[["Year", "Month", "Day"]],  # text that appears after latitude and longitude
-                         color_discrete_sequence=["red"])
+    fig = px.scatter_map(df, lat=df["Latitude"], lon=df["Longitude"], color=df["u_xy"],
+                         hover_name=df["u_xy"],  # text that appears over 
+                         hover_data=df[["Magnitude", "Year", "Month", "Day"]],  # text that appears after latitude and longitude
+                         )
     fig.update_layout(map_style="open-street-map")
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     fig.update_layout(map_bounds={"west": -180, "east": -80, "south": 10, "north": 90})
