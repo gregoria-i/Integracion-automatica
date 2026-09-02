3ce631402e07c1c13fb076a414ec747bffe5f35c
Gregoria <gm.andrea121@gmail.com>
2026-09-02 09:30:38 -0600

    Primer versión de contourf con Z función distinta de U, solo para preparar función de graficación

--- ZhuangOgata-2002/map_of_Mexico.py
+++ ZhuangOgata-2002/map_of_Mexico.py
@@ -11,6 +11,10 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 from scipy.interpolate import griddata
+
+# for contourf
+import geopandas as gpd
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -41,10 +45,29 @@ def show_results(df: pd.DataFrame):
     fig.update_layout(map_bounds={"west": -180, "east": -80, "south": 10, "north": 90})
     fig.show()
 
+def show_grid_results(shp_file):
+    plt.style.use('_mpl-gallery-nogrid')
+
+    gdf = gpd.read_file(shp_file)
+    #  print(gdf.crs)  # type of coordenates
+    xmin, ymin, xmax, ymax = gdf.total_bounds
+    X, Y = np.meshgrid(np.linspace(xmin, xmax, 256), np.linspace(ymin, ymax, 256))
+    Z = -X**2 - Y**2  # this cannot be the same value for all the grid, because levels needs increasing values
+
+    levels = np.linspace(Z.min(), Z.max())
+
+    fig, ax = plt.subplots()
+
+    ax.contourf(X, Y, Z, levels=levels)
+    
+    gdf.boundary.plot(ax=ax, color='black')
+    ax.set_aspect('equal')
+    plt.show()
+
 
 if __name__ == '__main__':
     earthquakes = "Earthquakes.csv"
     uxy = "U_xy.csv"
+    shp_mexico = "Mapa base a nivel estatal y mapa general. Formato Raster/mbtifgw.shp"
 
-    df = prepare_data(earthquakes, uxy, M0=4.3)
-    show_results(df)
+    show_grid_results(shp_mexico)
